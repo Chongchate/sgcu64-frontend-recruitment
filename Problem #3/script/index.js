9ae643c76d647723a13b3daf3dccbdb1c400e9d0
Ford <ford.chongchate@gmail.com>
2021-06-23 20:26:20 +0700

    Update index.js

--- Problem #3/script/index.js
+++ Problem #3/script/index.js
@@ -1,15 +1,39 @@
-const form = document.getElementById("register-form")
-form.addEventListener("submit", event => {
-  event.preventDefault()
-  const formData = new FormData(form)
-  const data = {}
+const form = document.querySelector("#register-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = {};
   for (const [key, value] of formData.entries()) {
     /* USER CODE Begin: Validate data */
-    data[key] = value
+    if (value === "") {
+      alert("Input cannot be empty");
+      return false;
+    }
+    if (key === "email") {
+      let atPosition = value.indexOf("@");
+      let dotPosition = value.lastIndexOf(".");
+      if (
+        atPosition < 1 ||
+        dotPosition < atPosition + 2 ||
+        dotPosition + 2 >= value.length
+      ) {
+        alert("Please enter a valid email address");
+        return false;
+      }
+    }
+
+    if (key === "confirmpassword") {
+      if (data["password"] !== value) {
+        alert("Confirm password does not match!");
+        return false;
+      }
+    }
+    data[key] = value;
+
     /* USER CODE Begin: Validate data */
   }
-  console.log(data)
+  console.log(data);
   /* USER CODE Begin: What happened next after recieve form data (Optional) */
 
   /* USER CODE END: What happened next after recieve form data (Optional) */
-})
+});
